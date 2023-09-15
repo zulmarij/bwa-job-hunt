46380a83069c8ff459f3f5ba3003c24f758d58f3
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2023-09-15 14:53:57 +0700

    slicing setup layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main>
+          <div className='border-t'>
+            <div className='bg-background'>
+              <div className='flex flex-row'>
+                <div className='hidden lg:block w-[18%]'>Sidebar</div>
+                <div className='col-span-3 overflow-auto lg:col-span-5 lg:border-l w-[82%]'>
+                  <div className='px-6 py-6 lg:px-8'>
+                    <div>Header</div>
+                    {children}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+      </body>
     </html>
   )
 }
