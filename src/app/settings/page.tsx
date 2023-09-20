36577044078efa78e9d88242fe71e8933b84c609
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2023-09-20 08:33:29 +0700

    slicing setting page

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,30 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { FC } from "react";
+
+interface SettingsPageProps {}
+
+const SettingsPage: FC<SettingsPageProps> = ({}) => {
+  return (
+    <div>
+      <div className="font-semibold text-3xl mb-5">Settings</div>
+      <Tabs defaultValue="overview">
+        <TabsList className="mb-8">
+          <TabsTrigger value="overview">Overview</TabsTrigger>
+          <TabsTrigger value="socialLinks">Social Links</TabsTrigger>
+          <TabsTrigger value="teams">Teams</TabsTrigger>
+        </TabsList>
+        <TabsContent value="overview">
+          <div>overview</div>
+        </TabsContent>
+        <TabsContent value="socialLinks">
+          <div>socialLinks</div>
+        </TabsContent>
+        <TabsContent value="teams">
+          <div>teams</div>
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+};
+
+export default SettingsPage;
